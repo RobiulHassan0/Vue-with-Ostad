bf460998253c371222036c16f22a0e343024d4d9
RobiulHassan0 <robiulhassan270@gmail.com>
2026-07-17 13:16:38 +0600

    two way data binding

---
+++ Batch-02/Section_03/two_way_data_binding/backup topics/05_data_binding_multiple_checkbox_array.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { reactive, ref } from 'vue';
+
+const favColors = ref([]); //not reactive. only use ref for multple value of an array
+
+</script>
+
+<template>
+
+  <section class="w-2/6 mt-10 mx-auto space-x-5">
+    <h1 class="text-2xl mb-10">Tow way data binding with Radio box</h1>
+    <p class="mb-10 ">{{ favColors }}</p>
+    Your Favourite Colors: <br>
+    <label for="red">Red</label>
+    <input type="checkbox" class="checkbox checkbox-secondary" value="red" v-model="favColors"/>
+    <label for="red">Green</label>
+    <input type="checkbox" class="checkbox checkbox-secondary" value="green" v-model="favColors"/>
+
+  </section>
+
+</template>
+
+<style scoped>
+
+</style>
--- Batch-02/Section_03/two_way_data_binding/src/App.vue
+++ Batch-02/Section_03/two_way_data_binding/src/App.vue
@@ -1,24 +1,58 @@
 <script setup>
 import { reactive, ref } from 'vue';
 
-const displayImage = ref(true);
+const asianCountries = reactive([
+  {name: 'Bangladesh', capital: 'Dhaka'},
+  {name: 'Pakistan', capital: 'Islamabad'},
+  {name: 'India', capital: 'New Delhi'},
+  {name: 'Afghanistan', capital: 'Kabul'},
+  {name: 'China', capital: 'Beijing'}
+]);
+
+const capitals= ['Dhaka', 'Kabul', 'Islamabad', 'Beijing', 'New Delhi']
+
+function getScore(){
+  let score = 0;
+  asianCountries.forEach( (country) => {
+    if(country.answer === country.capital){
+      score++;
+    }
+  });
+  return score;
+}
+
+// function getCapitalRandom(){
+//   return capitals;
+//   // return capitals.sort( () => Math.random() - 0.5)
+// }
 
 </script>
 
 <template>
+<section class="max-w-4xl mx-auto space-y-6 mt-4">
+  <h1 class="text-center">Vue Form</h1>
+  <div>
+      {{ asianCountries }}
+  </div>
 
-  <section class="w-2/6 mt-10 mx-auto space-x-5">
-    <h1 class="text-2xl mb-10">Tow way data binding with Radio box</h1>
-    <p class="mb-10 ">{{ displayImage }}</p>
-    <label for="">Display Random Image</label>
-    <input type="checkbox" class="checkbox checkbox-secondary"  v-model="displayImage"/>
+  <button class="btn btn-primary">Score: <strong class="text-2xl">{{ getScore() }}</strong></button>
 
-    <div class="flex gap-4 mt-4">
-      <input type="radio" name="display" class="radio radio-xl radio-secondary" :value="true" v-model="displayImage"/> ON 
-      <input type="radio" name="display" class="radio radio-xl radio-secondary" :value="false" v-model="displayImage" /> OFF
-    </div>
-    <img v-show="displayImage" class="mt-10 mx-auto w-175" :src="`https://picsum.photos/300/200?random=${Math.random()}`" alt="">
-  </section>
+  <div v-for="(country, index) in asianCountries" :key="country.name" class="border border-gray-300 flex flex-col gap-4 rounded-lg p-6 bg-white shadow-sm">
+      <h2 class="text-lg font-semibold text-gray-800 mb-5">
+          {{ index+1 }}. What is the capital of {{ country.name }}?
+      </h2>
+
+      <div class="flex justify-between gap-5">
+          <label v-for="(capital, index) in capitals" :key="index" class="flex items-center gap-2 cursor-pointer">
+              <input :value="capital" :name="country.name" v-model="country.answer" type="radio" name="radio-5" class="radio radio-secondary" />
+              <span class="text-black">{{ capital }}</span>
+          </label>
+
+      </div>
+  </div>
+
+
+</section>
 
 </template>
 
