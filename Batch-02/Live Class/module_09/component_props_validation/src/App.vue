dd14bf99f1c1ae9aafab3dbf26cee37cd5900a4c
RobiulHassan0 <robiulhassan270@gmail.com>
2026-08-02 07:36:54 +0600

    component learned

---
+++ Batch-02/Live Class/module_09/component_props_validation/src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import TeamSecttion from './views/TeamSection.vue';
+import { ref } from 'vue';
+
+const persons = ref([
+    {
+        name: "Shafin Jaman",
+        desgination: "CEO & Web Developer",
+        description: "Robiul Hassan the technical strategy of the flowbite platform and brand.",
+        avatar: "//flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png"
+    },
+    {
+        name: "Sadikur Rahman",
+        desgination: "Web Developer",
+        description: "Sadikur Rahman the technical strategy of the flowbite platform and brand.",
+        avatar: "//flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/joseph-mcfall.png"
+    },
+    {
+        name: "Adbul Karim",
+        desgination: "CEO & Founder",
+        description: "Adbul Karim the technical strategy of the flowbite platform and brand.",
+        avatar: "//flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/sofia-mcguire.png"
+    },
+    {
+        name: "Ibn Fulan",
+        desgination: "Software Engineer",
+        description: "Ibn Fulan the technical strategy of the flowbite platform and brand.",
+        avatar: "//flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gouch.png"
+    },
+
+])
+
+</script>
+
+<template>
+
+<TeamSecttion :persons="persons" />
+
+</template>
+
+<style scoped></style>
---
+++ Batch-02/Live Class/module_09/component_props_validation/src/views/TeamSection.vue
@@ -0,0 +1,30 @@
+<script setup>
+import Person from '@/components/Person.vue';
+
+defineProps({
+    persons: Array,
+})
+
+</script>
+
+<template>
+    <section class="flex justify-center">
+        <div class="bg-white dark:bg-gray-900">
+            <div class="flex flex-col gap-14 max-w-7xl py-16 px-6 ">
+                <div class="flex flex-col items-center justify-center gap-8 text-center ">
+                    <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Our Team</h2>
+                    <p class="font-light text-gray-500 mb-16 text-xl dark:text-gray-400">Explore the whole
+                        collection of open-source web components and elements built with the utility classes from
+                        Tailwind
+                    </p>
+                </div>
+                <div class="grid gap-8 mb-6 lg:mb-16 md:grid-cols-2">
+                    <Person v-for="(person, index) in persons" :key="index" :person="person" />
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+
+<style scoped></style>
